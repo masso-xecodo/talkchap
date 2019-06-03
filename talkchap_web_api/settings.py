e03955a5e6cce3a84c8bc67659e843c4ef36b435
massoxecodo <masso@xecodo.com>
2019-06-03 13:04:24 +0800

    fifth commit

--- talkchap_web_api/settings.py
+++ talkchap_web_api/settings.py
@@ -123,14 +123,21 @@ WSGI_APPLICATION = 'talkchap_web_api.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'talkchap',
+#         'USER': 'root',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'POST': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'talkchap',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'POST': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
